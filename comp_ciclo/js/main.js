4c81ede5d2c794eba42de5022f69bf771e4dbb64
Ramses Cortes <ramses.cortes87@gmail.com>
2022-04-30 10:57:18 -0500

    subida de desafio complementario de ciclos

---
+++ comp_ciclo/js/main.js
@@ -0,0 +1,9 @@
+let numero = prompt("Se captura un numero de repeticiones y por While se muestra en consola en cada renglon el numeor de repeticion");
+let repeticiones = 0;
+
+while(repeticiones<=numero){
+    console.log("Numero de repeticion ", repeticiones);
+    repeticiones++;
+}
+
+
